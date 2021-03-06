08de9a7dac287527dfc4422a0c1129bd4471aa34
jhihyu <a402250025@gmail.com>
2021-03-06 14:35:32 +0800

    chinese error

--- mblog/mainsite/models.py
+++ mblog/mainsite/models.py
@@ -9,4 +9,9 @@ class Post(models.Model):
     body = models.TextField()
     pub_date = models.DateTimeField(default=timezone.now)
     
+    class Meta:
+        ordering = ('-pub_date',)
+        
+    def __str__(self):
+        return self.title
     
